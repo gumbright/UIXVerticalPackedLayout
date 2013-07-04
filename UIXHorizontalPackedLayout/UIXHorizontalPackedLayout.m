7a061c78c2633460423044e52ad9f2ddb41e4d24
Guy Umbright <gumbright@gmail.com>
2013-07-03 22:48:08 -0500

    Basically have the workings of a more efficient allocation of attrs but it is currently for naught.  I had hoped that some means to determine the layoutSize would present itself that did not require fully calculating all the items.  No such luck.

--- UIXHorizontalPackedLayout/UIXHorizontalPackedLayout.m
+++ UIXHorizontalPackedLayout/UIXHorizontalPackedLayout.m
@@ -12,9 +12,116 @@
 #define ROWCACHEKEY_NUMBEROFITEMS @"numberOfItems"
 #define ROWCACHEKEY_NEXTROWITEMINDEX @"nextRowItemIndex"
 
+#pragma mark UIXPackedLayoutRowInfo
+
+@interface UIXPackedLayoutRowInfo : NSObject
+@property (nonatomic, assign) NSInteger startItemIndex;
+@property (nonatomic, assign) NSInteger nextRowItemIndex;
+@property (nonatomic, assign) NSInteger numberOfItems;
+@property (nonatomic, assign) CGFloat rowY;
+@property (nonatomic, strong) NSMutableArray* itemAttrs;
+
+- (NSArray*) attrsForRect:(CGRect) rect;
+@end
+
+@implementation UIXPackedLayoutRowInfo
+/////////////////////////////////////////////////////
+//
+/////////////////////////////////////////////////////
+- (id) init
+{
+    if (self = [super init])
+    {
+        self.itemAttrs = [NSMutableArray array];
+    }
+    
+    return self;
+}
+
+/////////////////////////////////////////////////////
+//
+/////////////////////////////////////////////////////
+- (NSArray*) attrsForRect:(CGRect) rect
+{
+    NSMutableArray* returnAttrs = [NSMutableArray array];
+    
+    for (UICollectionViewLayoutAttributes* attr in self.itemAttrs)
+    {
+        if (CGRectIntersectsRect(rect, attr.frame))
+        {
+            [returnAttrs addObject:attr];
+        }
+    }
+    
+    return returnAttrs;
+}
+
+@end
+
+#pragma mark -
+#pragma mark UIXPackedLayoutSectionInfo
+@interface UIXPackedLayoutSectionInfo : NSObject
+@property (nonatomic, assign) CGRect sectionRect;
+@property (nonatomic, strong) UICollectionViewLayoutAttributes* headerAttr;
+@property (nonatomic, assign) CGRect contentRect;
+@property (nonatomic, strong) NSMutableArray* rowInfo;
+
+- (NSArray*) attrsForRect:(CGRect) rect;
+@end
+
+@implementation UIXPackedLayoutSectionInfo
+/////////////////////////////////////////////////////
+//
+/////////////////////////////////////////////////////
+- (id) init
+{
+    if (self = [super init])
+    {
+        self.rowInfo = [NSMutableArray array];
+    }
+    
+    return self;
+}
+
+/////////////////////////////////////////////////////
+//
+/////////////////////////////////////////////////////
+- (CGRect) sectionRect
+{
+    CGRect rect;
+    
+    rect = self.headerAttr.frame;
+    rect = CGRectUnion(rect, self.contentRect);
+    
+    return rect;
+}
+
+/////////////////////////////////////////////////////
+//
+///////////////////////////////////////////////////
+- (NSArray*) attrsForRect:(CGRect) rect
+{
+    NSMutableArray* returnAttrs = [NSMutableArray array];
+    
+    if (self.headerAttr && CGRectIntersectsRect(self.headerAttr.frame, rect))
+    {
+        [returnAttrs addObject:self.headerAttr];
+    }
+
+    for (UIXPackedLayoutRowInfo* rowInfo in self.rowInfo)
+    {
+        [returnAttrs addObjectsFromArray:[rowInfo attrsForRect:rect]];
+    }
+    return returnAttrs;
+}
+@end
+
+#pragma mark -
+#pragma mark UIXHorizontalPackedLayout
 @interface UIXHorizontalPackedLayout ()
 @property (nonatomic, assign) CGFloat maxWidth;
-@property (nonatomic, strong) NSMutableDictionary* rowDataCache;
+//@property (nonatomic, strong) NSMutableDictionary* rowDataCache;
+@property (nonatomic, strong) NSMutableArray* sectionData;
 @end
 
 @implementation UIXHorizontalPackedLayout
@@ -26,7 +133,7 @@
     self.horizontalSpacing = 0;
     self.rowSpacing = 0;
     self.rowHeight = 100;
-    self.rowDataCache = [NSMutableDictionary dictionary];
+//    self.rowDataCache = [NSMutableDictionary dictionary];
 }
 
 /////////////////////////////////////////////////////
@@ -91,6 +198,9 @@
 /////////////////////////////////////////////////////
 - (void) prepareLayout
 {
+    self.sectionData = nil;
+    
+#if 0
     CGFloat currentX, currentY;
     NSUInteger numSections;
     NSUInteger numItems;
@@ -171,8 +281,10 @@
     self.layoutData = sectionData;
     self.headerData = headerData;
     self.layoutContentSize = CGSizeMake(self.collectionView.bounds.size.width,currentY-self.rowSpacing + self.sectionInset.bottom);
+#endif
 }
 
+#if 0
 //generate attribues for row
 //
 /////////////////////////////////////////////////////
@@ -219,20 +331,150 @@
         [self generateAttributesForRow:ndx];
     }
 }
+#endif
+
+/////////////////////////////////////////////////////
+//
+/////////////////////////////////////////////////////
+- (UIXPackedLayoutRowInfo*) generateRow:(NSInteger) startItemIndex inSection:(NSInteger) section numItems:(NSInteger) numItems startingY:(CGFloat) startingY
+{
+    CGSize sz;
+    CGFloat currentY = startingY;
+    CGFloat currentX = self.sectionInset.left;
+    NSMutableArray* currentRow = [NSMutableArray array];
+    
+    UIXPackedLayoutRowInfo* rowInfo = [[UIXPackedLayoutRowInfo alloc] init];
+    rowInfo.startItemIndex = startItemIndex;
+    rowInfo.rowY = startingY;
+    
+    int itemNdx;
+    for (itemNdx = startItemIndex; itemNdx < numItems; ++itemNdx)
+    {
+        sz = [self.delegate UIXPackedLayout: self sizeForItemAtIndex:[NSIndexPath indexPathForItem:itemNdx inSection:section]];
+        
+        if (currentX + sz.width > self.maxWidth)
+        {
+            break;
+        }
+
+        UICollectionViewLayoutAttributes* attr = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:[NSIndexPath indexPathForItem:itemNdx inSection:section]];
+        CGRect frame = CGRectMake(currentX, currentY, sz.width, sz.height);
+        attr.frame = frame;
+        [currentRow addObject:attr];
+        
+        currentX += (sz.width + self.horizontalSpacing);
+    }
+
+    if (self.justified)
+    {
+        [self justify:currentRow];
+    }
+    
+    rowInfo.numberOfItems = currentRow.count;
+    rowInfo.nextRowItemIndex = itemNdx;
+    [rowInfo.itemAttrs addObjectsFromArray:currentRow];
+    
+    return rowInfo;
+}
+
+/////////////////////////////////////////////////////
+//
+/////////////////////////////////////////////////////
+- (void) generateSection:(NSInteger) sectionNdx
+{
+    CGFloat currentY = 0.0;
+    
+    UIXPackedLayoutSectionInfo* sectionInfo = [[UIXPackedLayoutSectionInfo alloc] init];
+    
+    //!!!if section not 0, set the current y from prev sectoin
+    CGRect frame;
+    
+    NSIndexPath* indexPath = [NSIndexPath indexPathForItem:0 inSection:sectionNdx];
+    UICollectionViewLayoutAttributes* attr;
+    
+    CGSize sz = CGSizeZero;
+    if ([self.delegate respondsToSelector:@selector(UIXPackedLayout:sizeOfHeaderForSection:)])
+    {
+        sz = [self.delegate UIXPackedLayout:self sizeOfHeaderForSection:sectionNdx];
+    }
+    
+    if (sz.height != 0)
+    {
+        attr = [UICollectionViewLayoutAttributes layoutAttributesForSupplementaryViewOfKind:@"header" withIndexPath:indexPath];
+        frame = CGRectMake(self.sectionInset.left, currentY, sz.width, sz.height);
+        attr.frame = frame;
+        currentY += (frame.size.height + self.rowSpacing);
+        sectionInfo.headerAttr = attr;
+    }
+    
+    NSInteger numItems = [self.collectionView.dataSource collectionView:self.collectionView numberOfItemsInSection:sectionNdx];
+    for (int itemNdx = 0; itemNdx < numItems; ++itemNdx)
+    {
+        UIXPackedLayoutRowInfo* rowInfo = [self generateRow:itemNdx inSection:sectionNdx numItems:numItems startingY:currentY];
+        itemNdx = rowInfo.nextRowItemIndex;
+        [sectionInfo.rowInfo addObject:rowInfo];
+        currentY+= (self.rowHeight + self.rowSpacing);
+    }
+    
+    sectionInfo.contentRect = CGRectMake(0,CGRectGetMaxY(sectionInfo.headerAttr.frame),sz.width,(self.rowHeight * sectionInfo.rowInfo.count)+(self.rowSpacing * (sectionInfo.rowInfo.count - 1)));
+    [self.sectionData addObject:sectionInfo];
+}
 
 /////////////////////////////////////////////////////
 //
 /////////////////////////////////////////////////////
 - (NSArray*) layoutAttributesForElementsInRect:(CGRect)rect
 {
-    //determine starting and ending row
-    NSInteger startRow = rect.origin.y / self.rowHeight;
-    NSInteger endRow = (rect.origin.y + rect.size.height) / self.rowHeight;
+    //get # sections
+    NSInteger numSections = [self.collectionView.dataSource numberOfSectionsInCollectionView:self.collectionView];
+    BOOL haveStart = NO, haveEnd = NO;
+    NSInteger startSection, endSection;
     
-    [self prepareRowsUpTo:endRow];
-    //verify we have everything up to starting row
-    //for each row from start to end
-    //   generate attrs
+    int sectionNdx;
+    for (sectionNdx = 0; sectionNdx < numSections; ++sectionNdx)
+    {
+        if (self.sectionData.count < (sectionNdx + 1))
+        {
+            [self generateSection:sectionNdx];
+        }
+        
+        UIXPackedLayoutSectionInfo* sectionInfo = self.sectionData[sectionNdx];
+        if (!haveStart)
+        {
+            if (CGRectContainsPoint(sectionInfo.sectionRect, rect.origin))
+            {
+                haveStart = YES;
+                startSection = sectionNdx;
+            }
+        }
+        
+        if (!haveEnd)
+        {
+            if (CGRectContainsPoint(sectionInfo.sectionRect, CGPointMake(CGRectGetMaxX(rect), CGRectGetMaxY(rect))))
+            {
+                haveEnd = YES;
+                endSection = sectionNdx;
+            }
+        }
+        
+        if (haveStart && haveEnd)
+        {
+            break;
+        }
+    }
     
+    if (sectionNdx > numSections)
+    {
+        endSection = numSections -1;
+    }
+    
+    NSMutableArray* attrs = [NSMutableArray array];
+    for (int sectionNdx = startSection; sectionNdx <= endSection; ++sectionNdx)
+    {
+        UIXPackedLayoutSectionInfo* sectionInfo = self.sectionData[sectionNdx];
+        [attrs addObjectsFromArray: [sectionInfo attrsForRect:rect]];
+    }
+    
+    return attrs;
 }
 @end
